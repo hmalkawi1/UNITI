0092904808d48b1418d8237049ea50cd887e793c
Shawn Noruzi <snoruzi@lib3000-icmac19.ucslabs.sfu.ca>
2017-03-08 15:37:25 -0800

    slider grading implemented . png file fixed

--- UNITI/anxiety1ViewController.swift
+++ UNITI/anxiety1ViewController.swift
@@ -15,10 +15,66 @@ class anxiety1ViewController: UIViewController {
     
     
     // Set total score to zero
-    var totalAnxietyScore = 10
+    var totalAnxietyScore = 0
+    
+    var answer1 = 0
+    var answer2 = 0
+    var answer3 = 0
+    var answer4 = 0
+    var answer5 = 0
+    var answer6 = 0
+    var answer7 = 0
+    
     
     // Anxiety score label
     @IBOutlet weak var displayAnxietyScore: UILabel!
+    
+    @IBOutlet weak var Question1: UILabel!
+    
+    
+    @IBAction func Answer1(_ sender: UISlider) {
+        
+        answer1 = Int(sender.value)
+        Question1.text = String(answer1)
+  
+    }
+    
+    @IBOutlet weak var Question2: UILabel!
+    
+    
+    @IBAction func Answer2(_ sender: UISlider) {
+        answer2 = Int(sender.value)
+        Question2.text = String(answer2)
+    }
+
+
+    @IBOutlet weak var Question3: UILabel!
+    
+    @IBAction func Answer3(_ sender: UISlider) {
+        answer3 = Int(sender.value)
+        Question3.text = String(answer3)
+    }
+    
+    @IBOutlet weak var Question4: UILabel!
+    
+    @IBAction func Answer4(_ sender: UISlider) {
+        
+        answer4 = Int(sender.value)
+        Question4.text = String(answer4)
+        
+    }
+    
+    @IBOutlet weak var Question5: UILabel!
+    
+    @IBAction func Answer5(_ sender: UISlider) {
+        
+        answer5 = Int(sender.value)
+        Question5.text = String(answer5)
+        
+    }
+    
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,6 +88,9 @@ class anxiety1ViewController: UIViewController {
     
     // Calculate the anxiety score on button tap
     @IBAction func calculateAnxietyScore(_ sender: AnyObject) {
+        
+        totalAnxietyScore = answer7 + answer6 + answer5 + answer4 + answer3 + answer2 + answer1
+        
         // Evaluate score
         if totalAnxietyScore < 5 {
             displayAnxietyScore.text = "Mild Anxiety"
