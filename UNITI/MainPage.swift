46cd113447377ca5e7e2af4ed6ef77388040b089
Shawn Noruzi <snoruzi@lib3000-icmac28.ucslabs.sfu.ca>
2017-03-11 12:32:55 -0800

    Creating Split Storyboards

---
+++ UNITI/MainPage.swift
@@ -0,0 +1,13 @@
+//
+//  MainPage.swift
+//  UNITI
+//
+//  Created by Shawn Noruzi on 3/11/17.
+//  Copyright © 2017 Shawn Noruzi. All rights reserved.
+//
+
+import UIKit
+
+class MainPage: NSObject {
+
+}
