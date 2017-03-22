22561dea3bd545a88859af6895fdbb261f87453b
Mike Bassili <mbassili@lib3000-icmac22.local>
2017-03-22 14:50:24 -0700

    commented code, updated haptic ui

--- UNITI/TimerViewController.swift
+++ UNITI/TimerViewController.swift
@@ -1,36 +1,54 @@
-//
-//  ViewController.swift
-//  Timer
-//
-//  Created by Shawn Noruzi on 2017-03-06.
-//  Copyright © 2017 Shawn Noruzi. All rights reserved.
-//
+// ------------------------------------------------------ //
+// TimerViewController.swift                              //
+// Timer, Guided and Haptic Meditation                    //
+//                                                        //
+// Created by Mike and Homam on 2017-03-06.               //
+// Copyright © 2017  Mike and Homam. All rights reserved. //
+// ------------------------------------------------------ //
 
 import UIKit
 import AudioToolbox
 
 class ViewController: UIViewController {
     
+    // ----------------- //
+    // Haptic Meditation //
+    // ----------------- //
+    
+    // Todo
     
     
     
+    // ----------------- //
+    // Guided Meditation //
+    // ----------------- //
     
-    // The Timer
+    // Todo
+    
+    
+    
+    // ---------------- //
+    // Timed Meditation //
+    // ---------------- //
+    
+    // Timer meditation variables
     @IBOutlet weak var countingLabel: UILabel! // Display label
     var SwiftTimer = Timer() // Timer object
     var SwiftCounter = 0  // Counting label
     var PlayFlag = 0 // 0 if paused, 1 if running
     @IBOutlet weak var TimerDir: UILabel! // Instructions
     
+    // Timer session select slider
     @IBOutlet weak var timerSlider: UISlider!
     @IBAction func timerSlider(_ sender: UISlider) {
-        SwiftCounter = Int(sender.value)
-        countingLabel.text = String(SwiftCounter)
+        SwiftCounter = Int(sender.value) // Update var with slider value
+        countingLabel.text = String(SwiftCounter) // Update slider with save value
     }
     
     // Start button
     @IBAction func startButton(_ sender: AnyObject) {
-        if PlayFlag == 0{
+        // Check if counter is paused
+        if PlayFlag == 0 {
             PlayFlag = 1 // Trigger flag
             timerSlider.isHidden = true // Hide slider
             TimerDir.text = "Sit down and relax." // Change timer direction label
@@ -57,10 +75,12 @@ class ViewController: UIViewController {
     
     // Decrements counter
     func updateCounter() {
+        // Check to see if counter is currently active
         if SwiftCounter > 0 {
             SwiftCounter -= 1 // Increment counter val
             countingLabel.text = String(describing:SwiftCounter)
         }
+        // Handle a timer who's already counting
         else {
             PlayFlag = 0 // Trigger flag
             SwiftTimer.invalidate()
@@ -69,7 +89,11 @@ class ViewController: UIViewController {
     
     
     
+    // -------------- //
+    // Meta Functions //
+    // -------------- //
     
+    // Viewport load
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -79,6 +103,7 @@ class ViewController: UIViewController {
         
     }
 
+    // Garbage collection
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
